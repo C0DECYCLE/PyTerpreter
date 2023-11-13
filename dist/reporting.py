3ed2ab7b8242fd1926718fe93134c4fae950e4eb
Patertuck <115107324+Patertuck@users.noreply.github.com>
2023-11-13 15:30:00 +0100

    Tracing feature reporting (#32)
    * TRACING-FEATURE: Implement and add tracing logic
    
    * EXAMPLE-FEATURE: Add tracing logic test file
    
    * TRACING-BUGFIX: Make methods private
    
    * TRACING-BUGFIX: remove importing list, dict union
    
    ---------
    
    Co-authored-by: Patertuck <patic.brandao@hispeed.ch>

---
+++ dist/reporting.py
@@ -0,0 +1,82 @@
+import sys
+from datetime import datetime
+
+
+class TraceReporter:
+    def __init__(self, traceFile: str):
+        self.traceFile: str = traceFile
+        self.functions: dict[str, dict[str, [int | float | datetime]]] = {}
+
+        lines: list[str] = self.__readFile()
+        self.__processTraceData(lines)
+        self.__generateReport()
+
+    @staticmethod
+    def __calculatePadding(maxSeperatorLength: int) -> tuple[int, int]:
+        leftPadding: int = (maxSeperatorLength - len("Function Name")) // 2
+        rightPadding: int = maxSeperatorLength - len("Function Name") - leftPadding
+
+        if leftPadding + rightPadding != maxSeperatorLength:
+            rightPadding += 1
+        return leftPadding, rightPadding
+
+    def __readFile(self) -> list[str]:
+        with open(self.traceFile, "r") as file:
+            lines: list[str] = file.readlines()
+
+        return lines
+
+    def __processTraceData(self, lines: list[str]) -> None:
+        for line in lines:
+            functionID, funtionName, event, timestampStr = map(
+                str.strip, line.split(",")
+            )
+
+            timestamp: datetime = datetime.strptime(
+                timestampStr, "%Y-%m-%d %H:%M:%S.%f"
+            )
+
+            if funtionName not in self.functions:
+                self.functions[funtionName] = {
+                    "calls": 0,
+                    "totalTime": 0,
+                    "startTime": None,
+                }
+
+            if event == "start":
+                self.functions[funtionName]["startTime"] = timestamp
+            elif event == "stop":
+                startTime: datetime = self.functions[funtionName]["startTime"]
+                elapsedTime: float = (timestamp - startTime).total_seconds() * 1000
+                self.functions[funtionName]["totalTime"] += elapsedTime
+                self.functions[funtionName]["calls"] += 1
+
+    def __generateReport(self) -> None:
+        maxNameLength: int = max(len(name) for name in self.functions.keys())
+        maxSeperatorLength: int = max(maxNameLength, 13)
+
+        leftPadding, rightPadding = self.__calculatePadding(maxSeperatorLength)
+
+        print(
+            f"|{' ' * leftPadding}Function Name{' ' * rightPadding} | Num. of calls  |  Total Time (ms) |  Average Time (ms)  |"
+        )
+        print(
+            f"|{'-' * (maxSeperatorLength + 2)}|----------------|------------------|---------------------|"
+        )
+
+        for function_name, data in self.functions.items():
+            numCalls: int = data["calls"]
+            totalTime: float = data["totalTime"]
+            averageTime: float = totalTime / numCalls
+
+            print(
+                f"| {function_name:<{maxNameLength}} | {numCalls:<14} | {totalTime:<16.3f} | {averageTime:<19.3f} |"
+            )
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python reporting.py trace_file.log")
+    else:
+        traceFile: str = sys.argv[1]
+        reporter: TraceReporter = TraceReporter(traceFile)
